cf267339d004c99b274a14726753790fa3316678
Phan Kim Long <phanlong02.18@gmail.com>
2022-11-12 09:26:14 +0700

    Ver 1.1

--- Assignment/Assignment/Controllers/OrderController.cs
+++ Assignment/Assignment/Controllers/OrderController.cs
@@ -14,15 +14,68 @@ namespace Assignment.Controllers
         {
             using (var context = new FoodieDBContext())
             {
+                decimal totalPrice = 0;
                 String orderNo = context.Orders.Count().ToString();
-                Cart cart = context.Carts.FirstOrDefault(c => c.UserId == Id);
-                List<Product> products = context.Products.Where(p => p.ProductId == cart.ProductId).ToList();
-                foreach (Product product in products)
+                List<Cart> carts = context.Carts.Where(c => c.UserId == Id).ToList();
+                List<Payment> payments = context.Payments.Distinct().ToList();
+                foreach (Cart cart in carts)
                 {
-
+                    Product product = context.Products.FirstOrDefault(p => p.ProductId == cart.ProductId);
+                    cart.Product = product;
+                    totalPrice += decimal.Parse(cart.Product.Price.ToString());
                 }
-                List<Order> orders = context.Orders.Where(o => o.UserId == Id)
+                ViewBag.totalPrice = totalPrice;
+                ViewBag.cart = carts;
+                ViewBag.payments = payments;
+                ViewBag.userID = Id;
+                return View();
             }
+        }
+
+        public IActionResult CreateOrder()
+        {
+            string action = Request.Form["action"];
+            string raw_userId = Request.Form["userID"];
+            string raw_paymentId = Request.Form["pay"];
+            int userId = Int32.Parse(raw_userId);
+            int paymentId = Int32.Parse(raw_paymentId);
+            using(var context = new FoodieDBContext())
+            {
+                int orderNumber = context.Orders.Count() + 1;
+                String orderNo = orderNumber.ToString();
+
+                List<Cart> carts = context.Carts.Where(c => c.UserId == userId).ToList();
+                foreach (Cart cart in carts)
+                {
+                    Order order = new Order();
+                    Product product = context.Products.FirstOrDefault(p => p.ProductId == cart.ProductId);
+                    order.ProductId = product.ProductId;
+                    order.OrderNo = orderNo;
+                    order.Quantity = cart.Quantity;
+                    order.UserId = userId;
+                    order.Status = "Process";
+                    order.PaymentId = paymentId;
+                    order.OrderDate = DateTime.Now;
+                    context.Orders.Add(order);
+                    context.SaveChanges();
+                }
+                foreach (Cart cart in carts)
+                {
+                    context.Carts.Remove(cart);
+                    context.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult List()
+        {
+            using(var context = new FoodieDBContext())
+            {
+                List<Order> orders = context.Orders.ToList();
+                ViewBag.orders = orders;
+            }
+            return View();
         }
     }
 }
